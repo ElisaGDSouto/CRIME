3d75fe292f664200c336c0788ded9b0d59f158a4
ELISA SOUTO <99265708+ElisaGDSouto@users.noreply.github.com>
2022-03-01 08:04:13 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,51 @@
+fun main(args: Array<String>) {
+
+    println("Responda as perguntas com 1 para SIM ou 0 para NÃO")
+
+        val num1 = println("Telefonou para a vítima?")
+        readLine()!!.toFloat()
+        val num2 =  println("Esteve no local do crime?")
+        readLine()!!.toFloat()
+        val num3 = println("Mora perto da vítima?")
+        readLine()!!.toFloat()
+        val num4 = println("Devia para a vítima?")
+        readLine()!!.toFloat()
+        val num5 =println("Já trabalhou com a vítima?")
+        readLine()!!.toFloat()
+        fun somar(num1:Float, num2:Float, num3:Float, num4:Float, num5:Float,):Float {
+        return num1 + num2 + num3 + num4 + num5
+
+            if(somar() == 5){
+                else("assassino")
+                                }
+            if(somar() =>3||=4){
+                else("cúmplice")
+            }
+            if(somar() == 2){
+                else("suspeito")
+            }
+            if (1 == null) {
+                else ("Inocente")
+
+            }
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
---
+++ Respostas.kt
@@ -0,0 +1,14 @@
+public class Respostas {
+    val num1 = Respostas()
+    val num2 = Respostas()
+    val num3 = Respostas()
+    val num4 = Respostas()
+    val num5 = Respostas()
+
+  fun somar(num1:Float, num2:Float, num3:Float, num4:Float, num5:Float,):Float{
+      return num1 + num2 + num3 + num4 + num5
+  }
+
+
+
+}
